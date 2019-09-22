7af6670bad9b4e559cc3bad9147ebf48316efeeb
stuzzifr@gmail.com <stuzzifr@gmail.com>
2019-09-22 21:54:01 +0700

    new wheel

--- subscribe.php
+++ subscribe.php
@@ -1,26 +1,26 @@
 
 <?php
 
+
 $email = $_POST['email'];
 
-   $to_email = "desired@email.com";
-   $subject = "You hahe a new Subscriber";
-   $body =
-       '<b>Email: </b>' .  $email . '<br>' . '<br>'
-   ;
-   //echo $body;
-   $headers = "From: Your Website";
-   $headers .= "MIME-Version: 1.0\r\n";
-   $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$to_email = "marketing@immology.org";
+$subject = "You have a new Subscriber";
+$body =
+'<b>Email: </b>' .  $email . '<br>' . '<br>'
+;
+$headers = "From: new Immology subscriber ";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-   if ( mail($to_email, $subject, $body, $headers)) {
-    //echo("Email successfully sent to $to_email...");
-    header("Location: index.html");
+if ( mail($to_email, $subject, $body, $headers)) {
+  echo("Email successfully sent to $to_email...");
+  header("Location: index.html");
 
-   } else {
-     header("Location: index.html");
-      //echo("Email sending failed...");
-   }
+} else {
+ header("Location: index.html");
+ echo("Email sending failed...");
+}
 
 
 ?>
